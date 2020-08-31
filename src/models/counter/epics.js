ccab15da607fa642367b02236a593fbdac82db6c
kostispodaras <kostis.podaras@agileactors.com>
2020-08-31 12:16:23 +0300

    test commit

--- src/models/counter/epics.js
+++ src/models/counter/epics.js
@@ -3,21 +3,19 @@ import { ofType, combineEpics } from 'redux-observable';
 
 import { increment, decrement } from './actions';
 
-// mapTo(decrement,.type wont work. Cause it dispatch a string and not an object)
 const incrementEpic = (action$) =>
-  action$.pipe(
-    ofType(increment.type),
-    delay(2000),
-    mapTo({ type: decrement.type }),
-  );
+  action$.pipe(ofType(increment.type), delay(2000), mapTo(decrement()));
 
-const decrementEpic = (action$) =>
-  action$.pipe(
-    ofType(decrement.type),
-    delay(2000),
-    mapTo({ type: increment.type }),
-  );
+// const decrementEpic = (action$) =>
+//   action$.pipe(
+//     ofType(decrement.type),
+//     delay(2000),
+//     mapTo(decrement()),
+//   );
 
-const counterEpic = combineEpics(incrementEpic, decrementEpic);
+const counterEpic = combineEpics(
+  incrementEpic,
+  // decrementEpic
+);
 
 export { counterEpic };
